1480d902491d131bd5dfcfbed71ae1e33073cf86
Alex Fisher <alex@newknowledge.io>
2019-02-12 12:07:07 -0600

    default timeout to 1 second for redis 5.0

--- nk_queue/list_queue_client.py
+++ nk_queue/list_queue_client.py
@@ -12,8 +12,8 @@ class ListQueueClient(AbstractQueueClient):
     def connect(self):
         self._redis = redis.Redis(self._host, self._port, self._db)
 
-    def get(self, queue_name):
-        return self._redis.brpop(queue_name)
+    def get(self, queue_name, timeout=1):
+        return self._redis.brpop(queue_name, timeout=timeout)
 
     def put(self, queue_name, item):
         return self._redis.lpush(queue_name, item)
